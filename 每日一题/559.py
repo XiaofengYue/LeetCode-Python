a3e4db9a93c4af44b54dc08aea02c0f483acc874
YXF <736659711@qq.com>
2021-11-22 10:20:45 +0800

    DP

---
+++ 每日一题/559.py
@@ -0,0 +1,18 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Node
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        
+        return max(self.maxDepth(ch) for ch in root.children) + 1
