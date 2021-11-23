6b76c7e5fd4651e74b6ee7b4d298fbbc36a86bc4
YXF <736659711@qq.com>
2021-11-23 11:00:05 +0800

    简单DP问题

---
+++ 动态规划/53.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dp = [0] * len(nums)
+
+        dp[0] = nums[0]
+
+        for i in range(1, len(nums)):
+            dp[i] = max(dp[i-1]+nums[i], nums[i])
+        
+        return max(dp)
+
+s = Solution()
+s.maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
