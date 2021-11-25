b119af9afd56d9fac1aa6ff423edc1f9d16bf5ec
YXF <736659711@qq.com>
2021-11-25 10:23:19 +0800

    信息熵问题+最大信息量问题

---
+++ 每日一题/458.py
@@ -0,0 +1,20 @@
+# https://blog.csdn.net/believexfr/article/details/52823883
+class Solution(object):
+    def poorPigs(self, buckets, minutesToDie, minutesToTest):
+        """
+        :type buckets: int
+        :type minutesToDie: int
+        :type minutesToTest: int
+        :rtype: int
+        """
+        if buckets == 1:
+            return 0
+        pig_entropy = minutesToTest//minutesToDie + 1
+        i = 1
+        while True:
+            if pow(pig_entropy, i) >= buckets:
+                return i
+            i+=1
+
+s = Solution()
+s.poorPigs(1, 1, 1)
