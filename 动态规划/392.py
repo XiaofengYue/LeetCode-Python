a071eee90341e422f32e19ea51f1c3a6edf315e9
YXF <736659711@qq.com>
2021-12-09 15:10:12 +0800

    DP解法

--- 动态规划/392.py
+++ 动态规划/392.py
@@ -16,3 +16,27 @@ class Solution(object):
             if now == des:
                 return True
         return False
+
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        dp = [[False]*(len(s)+1) for i in range(len(t)+1)]
+        for i in range(len(t)):
+            dp[i+1][0] = True
+        if len(s) == 0:
+            return True
+        for i in range(len(t)):
+            for j in range(len(s)):
+                if dp[i][j+1]:
+                    dp[i+1][j+1] = True
+                elif t[i] == s[j] and dp[i+1][j]:
+                    dp[i+1][j+1] = True
+                    break
+        return dp[-1][-1]
+
+s = Solution()
+s.isSubsequence(s = "abb", t = "ahbgdc")
