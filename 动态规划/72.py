656a349eda73097b25d190fa4f7680722a5e4e8a
YXF <736659711@qq.com>
2021-12-09 17:23:59 +0800

    DP 编辑距离

--- 动态规划/72.py
+++ 动态规划/72.py
@@ -6,13 +6,16 @@ class Solution(object):
         :rtype: int
         """
         dp = [[0]*(len(word1)+1) for i in range(len(word2)+1)]
-
+        for i in range(len(word1)):
+            dp[0][i+1] = i+1
+        for i in range(len(word2)):
+            dp[i+1][0] = i+1
         for i in range(1, len(word2)+1):
             for j in range(1, len(word1)+1):
                 if word1[j-1] == word2[i-1]:
                     dp[i][j] = dp[i-1][j-1]
                 else:
-                    dp[i][j] = min(dp[i-1][j], dp[i][j-1])+1
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])+1
         return dp[-1][-1]
 
 s = Solution()
