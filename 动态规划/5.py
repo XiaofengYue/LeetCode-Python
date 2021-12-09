7b6eb257a05dcafbe0374c809db5ba5a11a9f91d
YXF <736659711@qq.com>
2021-12-09 10:59:03 +0800

    DP 尝试总结

--- 动态规划/5.py
+++ 动态规划/5.py
@@ -26,5 +26,29 @@ class Solution(object):
                         start = left
         return s[start:start+max_len]
 
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        dp = [[False] * len(s) for i in range(len(s))]
+        for i in range(len(s)):
+            dp[i][i] = True
+        max_len=1
+        index = 0
+        for end in range(1, len(s)):
+            for start in range(end):
+                if s[start] == s[end]:
+                    if end - start < 2:
+                        dp[start][end] = True
+                    else:
+                        dp[start][end] = dp[start+1][end-1]
+                if dp[start][end]:
+                    if end-start+1 > max_len:
+                        max_len = end-start+1
+                        index = start
+        return s[index:index+max_len]
+
 s = Solution()
 s.longestPalindrome("babad")
--- 动态规划/516.py
+++ 动态规划/516.py
@@ -14,5 +14,20 @@ class Solution(object):
                     dp[i][j]=max(dp[i+1][j],dp[i][j-1])
         return dp[0][-1]
 
+class Solution(object):
+    def longestPalindromeSubseq(self, s):
+        dp = [[1]*len(s) for i in range(len(s))]
+        for j in range(1, len(s)):
+            for i in range(j-1, -1, -1):
+                if s[i] == s[j]:
+                    if j - i < 2:
+                        dp[i][j] = 2
+                    else:
+                        dp[i][j] = dp[i+1][j-1] + 2
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+        return dp[0][-1]
+
+
 s = Solution()
 s.longestPalindromeSubseq('cbbdbcb')
